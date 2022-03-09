1f3ef78c86355e57d89925fa319d7815490ae9b2
Myke Rodas <mikealquimia@pitaya.tech>
2022-03-09 12:06:34 -0600

    first commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Field Change Activities",
-
-    'summary': """
-        ..""",
-
+    'summary': """..""",
     'description': """
         ..
     """,
-
     'author': "MARA Development",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '15.0.0',
-
-    # any module necessary for this one to work correctly
+    'category': 'Extra Tools',
+    'version': '15.1.1',
+    'data': [
+        'security/ir.model.access.csv',
+        #'views/views.xml',
+    ],
     'depends': ['base'],
-
 }
