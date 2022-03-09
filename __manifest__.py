6fc74c24f5689d608e403fefeab47eb5595425b0
Myke Rodas <mikealquimia@pitaya.tech>
2022-03-09 11:49:32 -0600

    add info in manifest

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Field Change Activities",
+
+    'summary': """
+        ..""",
+
+    'description': """
+        ..
+    """,
+
+    'author': "MARA Development",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '15.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+}
